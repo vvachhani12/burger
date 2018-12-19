6b9ce92467c3f46874cf55084064325e6f520c99
Vinit Vachhani <vinitvachhani@gmail.com>
2018-12-19 18:41:19 -0500

    final changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
